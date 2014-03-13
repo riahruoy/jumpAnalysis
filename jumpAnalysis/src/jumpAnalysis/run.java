bbcb51995a5e854d4f0318fa1f37a62978691596
Yohei FUJII <riahruoy.dev@gmail.com>
2014-03-13 11:58:23 +0900

    print jump duration and start time

--- jumpAnalysis/src/jumpAnalysis/run.java
+++ jumpAnalysis/src/jumpAnalysis/run.java
@@ -1,4 +1,8 @@
 package jumpAnalysis;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import jumpAnalysis.AccelData;
 import jumpAnalysis.outputItx;
 import jumpAnalysis.GpsData;
@@ -13,34 +17,45 @@ public class run{
     	
     	GpsData gpsdata = new GpsData();
     	gpsdata.read(dir + "location.gps.txt");
-        outputItx.writeItx(gpsdata.getT(), dir + "gps_t.itx", "gps_t");
-        outputItx.writeItx(gpsdata.getX(), dir + "gps_x.itx", "gps_x");
-        outputItx.writeItx(gpsdata.getY(), dir + "gps_y.itx", "gps_y");
-        outputItx.writeItx(gpsdata.getZ(), dir + "gps_z.itx", "gps_z");
+//        outputItx.writeItx(gpsdata.getT(), dir + "gps_t.itx", "gps_t");
+//        outputItx.writeItx(gpsdata.getX(), dir + "gps_x.itx", "gps_x");
+//        outputItx.writeItx(gpsdata.getY(), dir + "gps_y.itx", "gps_y");
+//        outputItx.writeItx(gpsdata.getZ(), dir + "gps_z.itx", "gps_z");
     	gpsdata.getSpeed(3.0, 1.0);
-    	outputItx.writeItx(gpsdata.getXbar(), dir + "gps_xbar.itx", "gps_xbar");
-        outputItx.writeItx(gpsdata.getYbar(), dir + "gps_ybar.itx", "gps_ybar");
-        outputItx.writeItx(gpsdata.getZbar(), dir + "gps_zbar.itx", "gps_zbar");
-    	outputItx.writeItx(gpsdata.getVx(), dir + "gps_vx.itx", "gps_vx");
-        outputItx.writeItx(gpsdata.getVy(), dir + "gps_vy.itx", "gps_vy");
-        outputItx.writeItx(gpsdata.getVz(), dir + "gps_vz.itx", "gps_vz");
-        outputItx.writeItx(gpsdata.getVamp(), dir + "gps_vamp.itx", "gps_vamp");
+//    	outputItx.writeItx(gpsdata.getXbar(), dir + "gps_xbar.itx", "gps_xbar");
+//        outputItx.writeItx(gpsdata.getYbar(), dir + "gps_ybar.itx", "gps_ybar");
+//        outputItx.writeItx(gpsdata.getZbar(), dir + "gps_zbar.itx", "gps_zbar");
+//    	outputItx.writeItx(gpsdata.getVx(), dir + "gps_vx.itx", "gps_vx");
+//        outputItx.writeItx(gpsdata.getVy(), dir + "gps_vy.itx", "gps_vy");
+//        outputItx.writeItx(gpsdata.getVz(), dir + "gps_vz.itx", "gps_vz");
+//        outputItx.writeItx(gpsdata.getVamp(), dir + "gps_vamp.itx", "gps_vamp");
     	
     	AccelData accdata = new AccelData();
         accdata.read(dir + "sensor.accelerometer.txt");
         accdata.detectJump(0.1 ,15.0, 8.0, 0.5);
-    	outputItx.writeItx(accdata.getX(), dir + "acc_x.itx", "acc_x");
-    	outputItx.writeItx(accdata.getY(), dir + "acc_y.itx", "acc_y");
-    	outputItx.writeItx(accdata.getZ(), dir + "acc_z.itx", "acc_z");
-    	outputItx.writeItx(accdata.getXsmth(), dir + "acc_x_smth.itx", "acc_x_smth");
-    	outputItx.writeItx(accdata.getYsmth(), dir + "acc_y_smth.itx", "acc_y_smth");
-    	outputItx.writeItx(accdata.getZsmth(), dir + "acc_z_smth.itx", "acc_z_smth");
-    	outputItx.writeItx(accdata.getamp(), dir + "acc_amp.itx", "acc_amp");
-    	outputItx.writeItx(accdata.getT(), dir + "acc_t.itx", "acc_t");
-        outputItx.writeItx(accdata.getDataSmth().getamp(), dir + "rslt_amp.itx", "rslt_amp");
-        outputItx.writeItx(accdata.getJump_t_start(), dir + "jump_t_start.itx", "jump_t_start");
-        outputItx.writeItx(accdata.getJump_t_duration(), dir + "Jump_t_duration.itx", "Jump_t_duration");
-        outputItx.writeItx(accdata.getJumpDistance(gpsdata.getVamp1d()), dir + "Jump_distance.itx", "Jump_distance");
+        for (double duration : accdata.getJump_t_duration()) {
+        	System.out.println("jump_duration : " + Double.valueOf(duration) + " s");
+        }
+        final SimpleDateFormat logTimeSdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", Locale.JAPAN);
+        for (double start : accdata.getJump_t_start()) {
+        	Date d = new Date((long)(start * 1000));
+        	System.out.println("jump_start : " + logTimeSdf.format(d));
+        }
+        for (double distance : accdata.getJumpDistance(gpsdata.getVamp1d())) {
+        	System.out.println("jump distance : " + Double.valueOf(distance) + " m");
+        }
+//    	outputItx.writeItx(accdata.getX(), dir + "acc_x.itx", "acc_x");
+//    	outputItx.writeItx(accdata.getY(), dir + "acc_y.itx", "acc_y");
+//    	outputItx.writeItx(accdata.getZ(), dir + "acc_z.itx", "acc_z");
+//    	outputItx.writeItx(accdata.getXsmth(), dir + "acc_x_smth.itx", "acc_x_smth");
+//    	outputItx.writeItx(accdata.getYsmth(), dir + "acc_y_smth.itx", "acc_y_smth");
+//    	outputItx.writeItx(accdata.getZsmth(), dir + "acc_z_smth.itx", "acc_z_smth");
+//    	outputItx.writeItx(accdata.getamp(), dir + "acc_amp.itx", "acc_amp");
+//    	outputItx.writeItx(accdata.getT(), dir + "acc_t.itx", "acc_t");
+//        outputItx.writeItx(accdata.getDataSmth().getamp(), dir + "rslt_amp.itx", "rslt_amp");
+//        outputItx.writeItx(accdata.getJump_t_start(), dir + "jump_t_start.itx", "jump_t_start");
+//        outputItx.writeItx(accdata.getJump_t_duration(), dir + "Jump_t_duration.itx", "Jump_t_duration");
+//        outputItx.writeItx(accdata.getJumpDistance(gpsdata.getVamp1d()), dir + "Jump_distance.itx", "Jump_distance");
 /*
         GyroData gyrodata = new GyroData ();
         gyrodata.read(dir + "sensor.gyroscope.txt");
